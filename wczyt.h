8cbec8f1425bac186b6fcf398da6eb9ddc97b7f9
Pawel <muchap1@ee.pw.edu.pl>
2015-04-12 16:13:36 +0200

    Poprawiono blad z generowaniem tekstu wyjsciowego. Działajace opcje -w -a -s -n, przechodzę do tworzenia plikow pośrednich.

--- wczyt.h
+++
@@ -1,9 +0,0 @@
-#ifndef _WCZYT_H_
-#define _WCZYT_H_
-FILE ** wczytaj_pliki_txt( char ** argv, int * n );
-
-void free_pliki( FILE ** pliki, int n ); 
-
-int format_plikow( char ** argv, int n );       /*zwraca 0 w przypadku plikow txt, 1 - pliki posrednie (format ind), -1 zly format*/
-
-#endif
--- zmienne.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ZMIENNE_H_
-#define _ZMIENNE_H_
-
-int ilosc_slow;
-int ilosc_ngramy;
-int wiersze;
-int rzad;
-
-#endif
